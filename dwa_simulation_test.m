bf80ec9fd2c06e549c2bd49bec71f8df30b9afd1
cyh <e1010473@u.nus.edu>
2022-11-10 23:03:15 +0800

    update

--- dwa_simulation_test.m
+++ dwa_simulation_test.m
@@ -53,28 +53,41 @@ area = [-1 11 -1 11];
 % [head_score_weight, distance_score_weight, speed_score_weight, forward time]
 evalParam = [0.05, 0.2, 0.1, 3.0];
 
-% results of trajectory point states
-result.x = [];
-tic;
-
+% initialize
 Xplus = zeros(5,5000);
-Xplus(:,1) = x;
-Pplus1 = 0.1;
+% Xplus(:,1) = x;
+Xplus(:,1) = [0.2,0.1,0,0,0];
+Pplus = [0.1    0 0 0 0;
+           0  0.1 0 0 0;
+           0    0 0 0 0;
+           0    0 0 0 0;
+           0    0 0 0 0];
+
+% results of trajectory point states
+resultReal.x = [x'];
+result.x = [Xplus(:,1)'];
+resultPredicted.x = [Xplus(:,1)'];
+resultObserved.x = [x(1:2)+normrnd(0,0.1,2,1)];
+resultSigma.x = [Pplus(1:2,1:2)];
+
+tic;
 
 % main loop
 for i=2:5000
     % required control signal u = [v, w] and trajectory 
     [u, traj] = DWA(x, kinematic, goal, evalParam, obstacle, obstacleR);
-    % cannot accurately get to the position
+    
     real_x = moveForward(x,u) + [normrnd(0,0.01,2,1);0;0;0];
+%     real_x = moveForward(x,u);
 
-    % prediction equation
+    % observed value by gps  
     gpsxy = [1 0 0 0 0;
-             0 1 0 0 0]*real_x + normrnd(0,0.05,2,1);
+             0 1 0 0 0]*real_x + normrnd(0,0.1,2,1);
+    resultObserved.x = [resultObserved.x, gpsxy];
 
     % EKF to estimate x,y
-    Q = [0.1   0 0 0 0;
-           0 0.1 0 0 0;
+    Q = [0.01   0 0 0 0;
+           0 0.01 0 0 0;
            0   0 0 0 0;
            0   0 0 0 0;
            0   0 0 0 0];
@@ -86,11 +99,23 @@ for i=2:5000
          0, 0,  1,                                 0,                   dt;
          0, 0,  0,                                 1,                    0;
          0, 0,  0,                                 0,                    1];
+    Xminus = moveForward(Xplus(:,i-1),u);
     
-
+    resultPredicted.x = [resultPredicted.x; Xminus'];
+    
+    Pminus = A*Pplus*A'+Q;
+    
+    % update
+    C = [1 0 0 0 0; 0 1 0 0 0];
+    K = Pminus*C'/(C*Pminus*C'+R);
+    Xplus(:,i) = Xminus+K*(gpsxy-[1 0 0 0 0; 0 1 0 0 0]*Xminus);
+    Pplus = (eye(5)-K*C)*Pminus;
+    x = Xplus(:,i);
 
     % store the trajectory
+    resultReal.x = [resultReal.x; real_x'];
     result.x = [result.x; x'];
+    resultSigma.x = [resultSigma.x;Pplus(1:2,1:2)];
 
     % reach the goal
     if norm(x(POS_X:POS_Y) - goal') < 0.1
@@ -101,7 +126,13 @@ for i=2:5000
     % Visualization
     hold off;
     ArrowLength = 0.5;
+    % robot real state
+    quiver(real_x(POS_X), real_x(POS_Y), ArrowLength*cos(real_x(YAW)), ArrowLength*sin(real_x(YAW)), 'ok'); 
+    hold on;
+    % robot estimated state
     quiver(x(POS_X), x(POS_Y), ArrowLength*cos(x(YAW)), ArrowLength*sin(x(YAW)), 'ok');  % robot
+    hold on;
+    plot(resultReal.x(:,POS_X), resultReal.x(:,POS_Y), '-k');   % plot the true trajectory points
     hold on;
     plot(result.x(:,POS_X), result.x(:,POS_Y), '-b');   % plot history trajectory points
     hold on;
@@ -121,7 +152,40 @@ for i=2:5000
     drawnow;    % refresh
 end
 toc % the running time
+%%
 
+figure;
+plot(resultReal.x(:,1),resultReal.x(:,2),'r','LineWidth',1);
+hold on;
+plot(resultPredicted.x(:,1),resultPredicted.x(:,2),'c','LineWidth',1);
+hold on;
+scatter(resultObserved.x(1,:),resultObserved.x(2,:),10,'g','filled');
+hold on;
+plot(result.x(:,1),result.x(:,2),'b','LineWidth',2);
+hold on;
+Ne = 500;% number of points in ellipse drawing
+inc= 2*pi/Ne;
+for t=1:i
+    pxy = [result.x(t,1),result.x(t,2)]';
+    phi= 0:inc:2*pi;
+    P = resultSigma.x(2*t-1:2*t,:);
+    sigmaScale = 3;
+    data = make_ellipse(pxy, P, sigmaScale, phi);
+    plot(data(1,:),data(2,:),'--y');
+    hold on;
+end
+legend('real','only predict','observed','estimated','estimated covariance');
+axis(area);
+grid on;
+
+%%
+error=[];
+for t=1:i-1
+    error=[error, norm(result.x(t,1)-resultReal.x(t,1), result.x(t,2)-resultReal.x(t,2))];
+end
+t=1:i-1;
+figure;
+plot(t,error,'r');
 
 
 
